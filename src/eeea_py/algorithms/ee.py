11fc82c0a86c699dab81eb10fc7a19efbafbb58a
Jd-IA <halowolf213@gmail.com>
2026-05-23 12:44:23 -0600

    Improvements in lb and ub in ee and adding EMNA

--- src/eeea_py/algorithms/ee.py
+++ src/eeea_py/algorithms/ee.py
@@ -1,80 +1,54 @@
 import numpy as np
+import tests
 
-
-def explicit_exploration(n, mode, fitness_fun, dim, lb, ub, tol, K):
+def explicit_exploration(fitness_fun, dim, lb, ub, n, tol, K, maxiter):
     """Returns a population of n individuals using explicit exploration.
 
     Keyword arguments:
-    n           -- number of individuals
-    mode        -- 'min' to minimize, 'max' to maximize the fitness_fun
     fitness_fun -- fitness function to explore
     dim         -- dimension of the fitness function
     lb          -- lower bound of the function
     ub          -- upper bound of the function
+    n           -- number of individuals
     tol         -- error tolerance
     K           -- number of consecutive generations below tolerance
+    maxiter     -- maximum number of iterations
     """
     # Generate initial random population P
     P0 = np.zeros((n, dim))
     for i in range(dim):
-        P0[:, i] = np.random.uniform(low=lb, high=ub, size=n)
+        P0[:, i] = np.random.uniform(low=lb[i], high=ub[i], size=n)
 
     # Evaluate population in the fitness function
-    Y = np.zeros(n)
-    for i in range(n):
-        Y[i] = fitness_fun(P0[i])
+    Y = np.apply_along_axis(fitness_fun, 1, P0)
 
     # Compute deciles
     Da = np.percentile(Y, np.arange(10, 101, 10))
 
     # Initialize error and generation counter to 0
-    E = tol + 1
     G = 0
+    n_gen = 1
 
-    while G < K:
+    while G < K and n_gen < maxiter:
         # Generate random population Q
         Q = np.zeros((n, dim))
         for i in range(dim):
-            Q[:, i] = np.random.uniform(low=lb, high=ub, size=n)
+            Q[:, i] = np.random.uniform(low=lb[i], high=ub[i], size=n)
 
         # Evaluate population in the fitness function
-        Z = np.zeros(n)
-        for i in range(n):
-            Z[i] = fitness_fun(Q[i])
+        Z = np.apply_along_axis(fitness_fun, 1, Q)
 
         # Merge populations P and Q
-        P_aux = np.zeros((P0.shape[0] + Q.shape[0], dim))
-        for i in range(P0.shape[0]):
-            for j in range(dim):
-                P_aux[i, j] = P0[i, j]
-        for i in range(Q.shape[0]):
-            for j in range(dim):
-                P_aux[P0.shape[0] + i, j] = Q[i, j]
-        P0 = P_aux
+        P0 = np.concatenate([P0, Q], axis=0)
 
         # Merge fitness arrays Y and Z
-        Y_aux = np.zeros(len(Y) + len(Z))
-        for i in range(len(Y)):
-            Y_aux[i] = Y[i]
-        for i in range(len(Z)):
-            Y_aux[len(Y) + i] = Z[i]
-        Y = Y_aux
+        Y = np.concatenate([Y, Z])
 
         # Compute deciles again
         Db = np.percentile(Y, np.arange(10, 101, 10))
 
         # Compute norm error
-        num = 0
-        for i in range(len(Da)):
-            num = num + (Da[i] - Db[i])**2
-        num = np.sqrt(num)
-
-        den = 0
-        for i in range(len(Da)):
-            den = den + Da[i]**2
-        den = np.sqrt(den)
-
-        E = num / den
+        E = np.linalg.norm(Da - Db) / np.linalg.norm(Da)
 
         # Check tolerance
         if E > tol:
@@ -84,15 +58,17 @@ def explicit_exploration(n, mode, fitness_fun, dim, lb, ub, tol, K):
 
         # Assign Db deciles to Da
         Da = Db
+        n_gen += 1
 
-    # Select the population by the best method
-    # selecting the n minimum or maximum individuals
-    if mode == "min":
-        ind = np.argsort(Y)
-        S = P0[ind[:n]]
-
-    elif mode == "max":
-        ind = np.argsort(Y)[::-1]
-        S = P0[ind[:n]]
+    # Select the n best individuals (minimum)
+    ind = np.argsort(Y)
+    S = P0[ind[:n]]
 
     return S
+
+#lb = np.full(2, -5.12)
+#ub = np.full(2, 5.12)
+
+#S = explicit_exploration(fitness_fun=tests.sphere, dim=2, lb=lb, ub=ub, n=50, tol=0.01, K=30, maxiter=300)
+
+#print("Population:", S)
